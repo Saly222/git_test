509fe44940d25e82b370a39bc092face0e81ee0d
Saly222 <139831340+Saly222@users.noreply.github.com>
2025-10-10 21:52:23 +1100

    Stop watch with Start, Stop, and Reset buttons and improved button styling

---
+++ 1_project_html_eg/script.js
@@ -0,0 +1,5 @@
+const startBtn = document.querySelector(".start");
+const resetBtn = document.querySelector(".reset");
+const stopBtn = document.querySelector(".stop");
+
+startBtn.addEventListener("cllck");
